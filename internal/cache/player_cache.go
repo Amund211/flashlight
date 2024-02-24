6f9b8817f53fe6ccffd8271b9cd0cc82b064a998
Amund Eggen Svandal <aesvandal@gmail.com>
2024-02-24 20:48:05 +0100

    Pass playercacheimpl by reference

--- internal/cache/player_cache.go
+++ internal/cache/player_cache.go
@@ -22,20 +22,20 @@ type playerCacheImpl struct {
 	cache *ttlcache.Cache[string, cachedResponse]
 }
 
-func (playerCache playerCacheImpl) getOrSet(uuid string, value cachedResponse) (cachedResponse, bool) {
+func (playerCache *playerCacheImpl) getOrSet(uuid string, value cachedResponse) (cachedResponse, bool) {
 	item, existed := playerCache.cache.GetOrSet(uuid, value)
 	return item.Value(), existed
 }
 
-func (playerCache playerCacheImpl) set(uuid string, data []byte, statusCode int) {
+func (playerCache *playerCacheImpl) set(uuid string, data []byte, statusCode int) {
 	playerCache.cache.Set(uuid, cachedResponse{data: data, statusCode: statusCode, valid: true}, ttlcache.DefaultTTL)
 }
 
-func (playerCache playerCacheImpl) delete(uuid string) {
+func (playerCache *playerCacheImpl) delete(uuid string) {
 	playerCache.cache.Delete(uuid)
 }
 
-func (playerCache playerCacheImpl) wait() {
+func (playerCache *playerCacheImpl) wait() {
 	time.Sleep(50 * time.Millisecond)
 }
 
@@ -45,6 +45,6 @@ func NewPlayerCache(ttl time.Duration) PlayerCache {
 		ttlcache.WithDisableTouchOnHit[string, cachedResponse](),
 	)
 	go playerTTLCache.Start()
-	return playerCacheImpl{cache: playerTTLCache}
+	return &playerCacheImpl{cache: playerTTLCache}
 }
 
