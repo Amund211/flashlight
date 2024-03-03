4ca43b22c983c3743be87e2402e33b3de7aac55b
Amund Eggen Svandal <aesvandal@gmail.com>
2024-03-03 18:18:10 +0100

    tests: writeErrorResponse

--- internal/server/errors.go
+++ internal/server/errors.go
@@ -9,24 +9,14 @@ import (
 	e "github.com/Amund211/flashlight/internal/errors"
 )
 
-func writeErrorResponse(w http.ResponseWriter, err error) {
-	if errors.Is(err, e.APIServerError) {
-		w.WriteHeader(http.StatusInternalServerError)
-	} else if errors.Is(err, e.APIClientError) {
-		w.WriteHeader(http.StatusBadRequest)
-	} else if errors.Is(err, e.RatelimitExceededError) {
-		w.WriteHeader(http.StatusTooManyRequests)
-	} else {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
+func writeErrorResponse(w http.ResponseWriter, responseError error) {
+	w.Header().Set("Content-Type", "application/json")
 
 	errorResponse := HypixelAPIErrorResponse{
 		Success: false,
-		Cause:   err.Error(),
+		Cause:   responseError.Error(),
 	}
-
 	errorBytes, err := json.Marshal(errorResponse)
-
 	if err != nil {
 		log.Println("Error marshalling error response: %w", err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -34,5 +24,17 @@ func writeErrorResponse(w http.ResponseWriter, err error) {
 		return
 	}
 
+	// Unknown error: default to 500
+	statusCode := http.StatusInternalServerError
+
+	if errors.Is(responseError, e.APIServerError) {
+		statusCode = http.StatusInternalServerError
+	} else if errors.Is(responseError, e.APIClientError) {
+		statusCode = http.StatusBadRequest
+	} else if errors.Is(responseError, e.RatelimitExceededError) {
+		statusCode = http.StatusTooManyRequests
+	}
+
+	w.WriteHeader(statusCode)
 	w.Write(errorBytes)
 }
