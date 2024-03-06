6e6decfbc554a5214d924551d2a49eb3222d3ab4
Amund Eggen Svandal <aesvandal@gmail.com>
2024-03-06 18:50:02 +0100

    chore: Remove unused errors
    These do not happen in our code, but can be forwarded by a successful
    request to hypixel.

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -5,7 +5,5 @@ import "errors"
 var (
 	APIServerError         = errors.New("Server error")
 	APIClientError         = errors.New("Client error")
-	APIKeyError            = errors.New("Invalid API key")
-	PlayerNotFoundError    = errors.New("Player not found")
 	RatelimitExceededError = errors.New("Ratelimit exceeded")
 )
