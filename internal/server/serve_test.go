988b4547850ed329a34785ed77f363d75142a242
Amund Eggen Svandal <aesvandal@gmail.com>
2024-03-09 23:22:02 +0100

    chore: Test MakeServe

---
+++ internal/server/serve_test.go
@@ -0,0 +1,64 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	e "github.com/Amund211/flashlight/internal/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMakeServeGetPlayerData(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		t.Parallel()
+		serveGetPlayerData := MakeServeGetPlayerData(func(uuid string) ([]byte, int, error) {
+			return []byte(`data`), 200, nil
+		})
+		w := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, "/?uuid=uuid1234", nil)
+		serveGetPlayerData(w, req)
+
+		resp := w.Result()
+
+		assert.Equal(t, 200, resp.StatusCode, "Expected 200, got %d", resp.StatusCode)
+		assert.Equal(t, `data`, w.Body.String(), "Expected 'data', got %s", w.Body.String())
+		contentType := resp.Header.Get("Content-Type")
+		assert.Equal(t, "application/json", contentType, "Expected 'application/json', got %s", contentType)
+	})
+
+	t.Run("client error", func(t *testing.T) {
+		t.Parallel()
+		serveGetPlayerData := MakeServeGetPlayerData(func(uuid string) ([]byte, int, error) {
+			return []byte(``), -1, fmt.Errorf("%w: error :^)", e.APIClientError)
+		})
+		w := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, "/?uuid=uuid1234", nil)
+
+		serveGetPlayerData(w, req)
+
+		resp := w.Result()
+		assert.Equal(t, 400, resp.StatusCode, "Expected 400, got %d", resp.StatusCode)
+		assert.Equal(t, `{"success":false,"cause":"Client error: error :^)"}`, w.Body.String())
+		contentType := resp.Header.Get("Content-Type")
+		assert.Equal(t, "application/json", contentType, "Expected 'application/json', got %s", contentType)
+	})
+
+	t.Run("server error", func(t *testing.T) {
+		t.Parallel()
+		serveGetPlayerData := MakeServeGetPlayerData(func(uuid string) ([]byte, int, error) {
+			return []byte(``), -1, fmt.Errorf("%w: error :^(", e.APIServerError)
+		})
+		w := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, "/?uuid=uuid1234", nil)
+
+		serveGetPlayerData(w, req)
+
+		resp := w.Result()
+		assert.Equal(t, 500, resp.StatusCode, "Expected 500, got %d", resp.StatusCode)
+		assert.Equal(t, `{"success":false,"cause":"Server error: error :^("}`, w.Body.String())
+		contentType := resp.Header.Get("Content-Type")
+		assert.Equal(t, "application/json", contentType, "Expected 'application/json', got %s", contentType)
+	})
+}
