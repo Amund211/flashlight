2e90aac5c601aab431840281c1af1ed0cdca5c75
Amund Eggen Svandal <aesvandal@gmail.com>
2024-03-10 14:59:50 +0100

    refactor: Use http.HandlerFunc

--- internal/server/types.go
+++ internal/server/types.go
@@ -1,11 +1,5 @@
 package server
 
-import (
-	"net/http"
-)
-
-type Handler func(w http.ResponseWriter, r *http.Request)
-
 type HypixelAPIErrorResponse struct {
 	Success bool   `json:"success"`
 	Cause   string `json:"cause"`
