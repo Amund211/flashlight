1bd196cc9187b722a74627c056a68e2e6eff3664
Amund Eggen Svandal <aesvandal@gmail.com>
2024-05-16 17:48:10 +0200

    feat: Add ComposeMiddlewares

--- internal/server/middleware_test.go
+++ internal/server/middleware_test.go
@@ -57,3 +57,106 @@ func TestRateLimitMiddleware(t *testing.T) {
 		runTest(t, false)
 	})
 }
+
+func TestComposeMiddlewares(t *testing.T) {
+	t.Run("single middleware", func(t *testing.T) {
+		handlerCalled := false
+		middlewareStage := "not called"
+		middleware := ComposeMiddlewares(
+			func(next http.HandlerFunc) http.HandlerFunc {
+				return func(w http.ResponseWriter, r *http.Request) {
+					middlewareStage = "pre"
+					next(w, r)
+					middlewareStage = "post"
+				}
+			},
+		)
+
+		handler := middleware(
+			func(w http.ResponseWriter, r *http.Request) {
+				handlerCalled = true
+				assert.Equal(t, "pre", middlewareStage)
+			},
+		)
+
+		w := httptest.NewRecorder()
+		handler(w, &http.Request{})
+
+		assert.True(t, handlerCalled)
+		assert.Equal(t, "post", middlewareStage)
+	})
+
+	t.Run("multiple middleware", func(t *testing.T) {
+		handlerCalled := false
+
+		stage1 := "not called"
+		stage2 := "not called"
+		stage3 := "not called"
+
+		middleware1 := func(next http.HandlerFunc) http.HandlerFunc {
+			return func(w http.ResponseWriter, r *http.Request) {
+				assert.Equal(t, "not called", stage1)
+				assert.Equal(t, "not called", stage2)
+				assert.Equal(t, "not called", stage3)
+
+				stage1 = "pre"
+				next(w, r)
+				stage1 = "post"
+
+				assert.Equal(t, "post", stage1)
+				assert.Equal(t, "post", stage2)
+				assert.Equal(t, "post", stage3)
+			}
+		}
+		middleware2 := func(next http.HandlerFunc) http.HandlerFunc {
+			return func(w http.ResponseWriter, r *http.Request) {
+				assert.Equal(t, "pre", stage1)
+				assert.Equal(t, "not called", stage2)
+				assert.Equal(t, "not called", stage3)
+
+				stage2 = "pre"
+				next(w, r)
+				stage2 = "post"
+
+				assert.Equal(t, "pre", stage1)
+				assert.Equal(t, "post", stage2)
+				assert.Equal(t, "post", stage3)
+			}
+		}
+		middleware3 := func(next http.HandlerFunc) http.HandlerFunc {
+			return func(w http.ResponseWriter, r *http.Request) {
+				assert.Equal(t, "pre", stage1)
+				assert.Equal(t, "pre", stage2)
+				assert.Equal(t, "not called", stage3)
+
+				stage3 = "pre"
+				next(w, r)
+				stage3 = "post"
+
+				assert.Equal(t, "pre", stage1)
+				assert.Equal(t, "pre", stage2)
+				assert.Equal(t, "post", stage3)
+			}
+		}
+
+		middleware := ComposeMiddlewares(middleware1, middleware2, middleware3)
+
+		handler := middleware(
+			func(w http.ResponseWriter, r *http.Request) {
+				assert.Equal(t, "pre", stage1)
+				assert.Equal(t, "pre", stage2)
+				assert.Equal(t, "pre", stage3)
+				handlerCalled = true
+			},
+		)
+
+		w := httptest.NewRecorder()
+		handler(w, &http.Request{})
+
+		assert.True(t, handlerCalled)
+
+		assert.Equal(t, "post", stage1)
+		assert.Equal(t, "post", stage2)
+		assert.Equal(t, "post", stage3)
+	})
+}
