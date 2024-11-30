071fc5268f45da7d88be9424ce0760eae258ef10
Amund Eggen Svandal <aesvandal@gmail.com>
2024-11-30 14:08:24 +0100

    chore: Add strutils.NormalizeUUID

---
+++ internal/strutils/uuid.go
@@ -0,0 +1,38 @@
+package strutils
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+const VALID_HEX_DIGITS = "0123456789abcdefABCDEF"
+
+const STRIPPED_UUID_LENGTH = 32
+
+// Removes dashes and converts all characters to lowercase
+func NormalizeUUID(uuid string) (string, error) {
+	var normalized strings.Builder
+	builderCap := normalized.Cap()
+	missingCap := STRIPPED_UUID_LENGTH - builderCap
+	if missingCap > 0 {
+		normalized.Grow(missingCap)
+	}
+
+	for _, char := range uuid {
+		if char == '-' {
+			continue
+		} else if strings.ContainsRune(VALID_HEX_DIGITS, char) {
+			_, err := normalized.WriteRune(unicode.ToLower(char))
+			if err != nil {
+				return "", fmt.Errorf("failed writing to stringbuilder: %w", err)
+			}
+		} else {
+			return "", fmt.Errorf("invalid character in UUID. input: '%s'", uuid)
+		}
+	}
+	if normalized.Len() != STRIPPED_UUID_LENGTH {
+		return "", fmt.Errorf("normalized UUID is not 32 characters long. input: '%s'", uuid)
+	}
+	return normalized.String(), nil
+}
---
+++ internal/strutils/uuid_test.go
@@ -0,0 +1,104 @@
+package strutils_test
+
+import (
+	"testing"
+
+	"github.com/Amund211/flashlight/internal/strutils"
+	"github.com/stretchr/testify/require"
+)
+
+const INVALID_CHARACTER = "invalid character in UUID"
+const BAD_LENGTH = "normalized UUID is not 32 characters long"
+
+func TestNormalizeUUID(t *testing.T) {
+	cases := []struct {
+		input          string
+		expected       string
+		errorSubstring string
+	}{
+		{
+			// Regular dashed UUID
+			input:    "01234567-89ab-cdef-0123-456789abcdef",
+			expected: "0123456789abcdef0123456789abcdef",
+		},
+		{
+			// All caps dashed UUID
+			input:    "01234567-89AB-CDEF-0123-456789ABCDEF",
+			expected: "0123456789abcdef0123456789abcdef",
+		},
+		{
+			// Mixed case dashed UUID
+			input:    "01234567-89ab-cdef-0123-456789ABCDEF",
+			expected: "0123456789abcdef0123456789abcdef",
+		},
+		{
+			// Regular stripped UUID
+			input:    "0123456789abcdef0123456789abcdef",
+			expected: "0123456789abcdef0123456789abcdef",
+		},
+		{
+			// All caps stripped UUID
+			input:    "0123456789ABCDEF0123456789ABCDEF",
+			expected: "0123456789abcdef0123456789abcdef",
+		},
+		{
+			// Mixed case stripped UUID
+			input:    "0123456789ABCDEF0123456789abcdef",
+			expected: "0123456789abcdef0123456789abcdef",
+		},
+		{
+			// Partially stripped UUID
+			input:    "01234567-89abcdef-0123456789abcdef",
+			expected: "0123456789abcdef0123456789abcdef",
+		},
+		{
+			// Weird dashes
+			input:    "---------0123---------4567-89------------------abcdef-012345---------6789abcdef---------",
+			expected: "0123456789abcdef0123456789abcdef",
+		},
+		{
+			input:          "0123456789ABCDEF0123456789abcdex",
+			errorSubstring: INVALID_CHARACTER,
+		},
+		{
+			input:          "0123456789xBCDEF0123456789abcdef",
+			errorSubstring: INVALID_CHARACTER,
+		},
+		{
+			// Too long
+			input:          "01234567-89ab-cdef-0123-456789abcdef-0",
+			errorSubstring: BAD_LENGTH,
+		},
+		{
+			// Too short
+			input:          "01234567-89ab-cdef-0123-456789abcde",
+			errorSubstring: BAD_LENGTH,
+		},
+	}
+
+	for _, input := range `—ghijklmnopqrstuvwxyzGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+[]{}|;':",.<>?/` {
+		cases = append(cases, struct {
+			input          string
+			expected       string
+			errorSubstring string
+		}{
+			input:          string(input),
+			errorSubstring: INVALID_CHARACTER,
+		})
+	}
+
+	for _, tc := range cases {
+		require.True(t, tc.expected != "" || tc.errorSubstring != "", "test case must expect a value or an error")
+		t.Run(tc.input, func(t *testing.T) {
+			actual, err := strutils.NormalizeUUID(tc.input)
+
+			if tc.errorSubstring != "" {
+				require.Contains(t, err.Error(), tc.errorSubstring)
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, actual)
+		})
+	}
+}
