a96a2f7a8a49d11dc0516b49cdbbaa62e2578493
Amund Eggen Svandal <aesvandal@gmail.com>
2024-11-30 15:28:27 +0100

    chore: Include dashes in normalized UUIDs
    This is the canonical representation of UUIDS, and also makes them a bit
    easier to read :^)

--- internal/strutils/uuid_test.go
+++ internal/strutils/uuid_test.go
@@ -19,42 +19,42 @@ func TestNormalizeUUID(t *testing.T) {
 		{
 			// Regular dashed UUID
 			input:    "01234567-89ab-cdef-0123-456789abcdef",
-			expected: "0123456789abcdef0123456789abcdef",
+			expected: "01234567-89ab-cdef-0123-456789abcdef",
 		},
 		{
 			// All caps dashed UUID
 			input:    "01234567-89AB-CDEF-0123-456789ABCDEF",
-			expected: "0123456789abcdef0123456789abcdef",
+			expected: "01234567-89ab-cdef-0123-456789abcdef",
 		},
 		{
 			// Mixed case dashed UUID
 			input:    "01234567-89ab-cdef-0123-456789ABCDEF",
-			expected: "0123456789abcdef0123456789abcdef",
+			expected: "01234567-89ab-cdef-0123-456789abcdef",
 		},
 		{
 			// Regular stripped UUID
 			input:    "0123456789abcdef0123456789abcdef",
-			expected: "0123456789abcdef0123456789abcdef",
+			expected: "01234567-89ab-cdef-0123-456789abcdef",
 		},
 		{
 			// All caps stripped UUID
 			input:    "0123456789ABCDEF0123456789ABCDEF",
-			expected: "0123456789abcdef0123456789abcdef",
+			expected: "01234567-89ab-cdef-0123-456789abcdef",
 		},
 		{
 			// Mixed case stripped UUID
 			input:    "0123456789ABCDEF0123456789abcdef",
-			expected: "0123456789abcdef0123456789abcdef",
+			expected: "01234567-89ab-cdef-0123-456789abcdef",
 		},
 		{
 			// Partially stripped UUID
 			input:    "01234567-89abcdef-0123456789abcdef",
-			expected: "0123456789abcdef0123456789abcdef",
+			expected: "01234567-89ab-cdef-0123-456789abcdef",
 		},
 		{
 			// Weird dashes
 			input:    "---------0123---------4567-89------------------abcdef-012345---------6789abcdef---------",
-			expected: "0123456789abcdef0123456789abcdef",
+			expected: "01234567-89ab-cdef-0123-456789abcdef",
 		},
 		{
 			input:          "0123456789ABCDEF0123456789abcdex",
