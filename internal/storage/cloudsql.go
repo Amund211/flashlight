7c23c50467b7501beb6633f0456bc41b6dc04b4e
Amund Eggen Svandal <aesvandal@gmail.com>
2024-11-30 10:20:42 +0100

    chore: Add cloudsql connection string

---
+++ internal/storage/cloudsql.go
@@ -0,0 +1,14 @@
+package storage
+
+import "fmt"
+
+// https://cloud.google.com/sql/docs/mysql/connect-functions
+func GetCloudSQLConnectionString(dbUsername, dbPassword, unixSocketPath string) string {
+	return fmt.Sprintf(
+		"user=%s password=%s database=%s host=%s",
+		dbUsername,
+		dbPassword,
+		DB_NAME,
+		unixSocketPath,
+	)
+}
