b91d0ca0a50b5983d0ecf944e13c7ec8e8b517f3
Amund Eggen Svandal <aesvandal@gmail.com>
2024-12-06 22:08:34 +0100

    chore: Add tests for migrating up and down
    Up migrations were already tested when setting up for testing the
    persistor, but down migrations were not. Copied the migration logic from
    the migrator, but the most important thing is that the migrations
    contain valid sql, and actually run when we need them to.

---
+++ internal/storage/migrator_test.go
@@ -0,0 +1,61 @@
+package storage
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+	"testing"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
+	"github.com/lib/pq"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMigrator(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping migrator tests in short mode.")
+	}
+
+	t.Run("migrate up and down", func(t *testing.T) {
+		ctx := context.Background()
+		schemaName := "migrate_up_down"
+
+		db, err := NewPostgresDatabase(LOCAL_CONNECTION_STRING)
+		require.NoError(t, err)
+
+		db.MustExec(fmt.Sprintf("DROP SCHEMA IF EXISTS %s CASCADE", pq.QuoteIdentifier(schemaName)))
+
+		logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+		migrator := NewDatabaseMigrator(db, logger)
+
+		err = migrator.migrate(ctx, schemaName)
+		require.NoError(t, err, "error migrating up")
+
+		// Migrate down manually
+		conn, err := db.Conn(ctx)
+		require.NoError(t, err)
+		defer conn.Close()
+		_, err = conn.ExecContext(ctx, fmt.Sprintf("SET search_path TO %s", pq.QuoteIdentifier(schemaName)))
+		require.NoError(t, err)
+
+		migrationSource, err := iofs.New(embeddedMigrations, "migrations")
+		require.NoError(t, err)
+		defer migrationSource.Close()
+
+		dbDriver, err := postgres.WithConnection(ctx, conn, &postgres.Config{
+			DatabaseName: DB_NAME,
+			SchemaName:   schemaName,
+		})
+		require.NoError(t, err)
+
+		migratorInstance, err := migrate.NewWithInstance("iofs", migrationSource, "postgres", dbDriver)
+		require.NoError(t, err)
+		defer migratorInstance.Close()
+
+		err = migratorInstance.Down()
+		require.NoError(t, err, "error migrating down") // Should not even be ErrNoChange
+	})
+}
