e746f2543a3ec415ebbcb05c375be7068a0f3d28
Amund Eggen Svandal <aesvandal@gmail.com>
2025-04-22 20:00:19 +0200

    chore: Make TTL cache purely generic
    Add helper to instantiate a player cache

--- internal/adapters/cache/player.go
+++ internal/adapters/cache/player.go
@@ -1,8 +1,14 @@
 package cache
 
+import "time"
+
 type playerResponse struct {
 	data       []byte
 	statusCode int
 }
 
 type PlayerCache = Cache[playerResponse]
+
+func NewTTLPlayerCache(ttl time.Duration) Cache[playerResponse] {
+	return NewTTLCache[playerResponse](ttl)
+}
