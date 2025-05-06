6842f1375e230309d839d4aa045c38dfd87fbbe8
Amund Eggen Svandal <aesvandal@gmail.com>
2025-05-06 20:41:57 +0200

    chore: Add comment about errors raised from player provider

--- internal/adapters/playerprovider/interface.go
+++ internal/adapters/playerprovider/interface.go
@@ -7,5 +7,8 @@ import (
 )
 
 type PlayerProvider interface {
+	// Raises domain.ErrPlayerNotFound if no player data is found for the given UUID
+	//
+	// Raises domain.ErrTemporarilyUnavailable if the provider implementation receives an error believed to be intermittent. The call may be retried later.
 	GetPlayer(ctx context.Context, uuid string) (*domain.PlayerPIT, error)
 }
