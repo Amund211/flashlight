Amund211/flashlight

commit 611f554c69a51f154996021d9dcbad692cdb2203

chore: userId -> userID

internal/ports/player_data.go
@@ -28,13 +28,13 @@ func MakeGetPlayerDataHandler(
 		),
 		ratelimiting.IPKeyFunc,
 	)
-	userIdRateLimiter := ratelimiting.NewRequestBasedRateLimiter(
+	userIDRateLimiter := ratelimiting.NewRequestBasedRateLimiter(
 		// NOTE: Rate limiting based on user controlled value
 		ratelimiting.NewTokenBucketRateLimiter(
 			ratelimiting.RefillPerSecond(2),
 			ratelimiting.BurstSize(120),
 		),
-		ratelimiting.UserIdKeyFunc,
+		ratelimiting.UserIDKeyFunc,
 	)
 
 	makeOnLimitExceeded := func(rateLimiter ratelimiting.RequestRateLimiter) http.HandlerFunc {
@@ -56,19 +56,19 @@ func MakeGetPlayerDataHandler(
 		logging.NewRequestLoggerMiddleware(logger),
 		sentryMiddleware,
 		NewRateLimitMiddleware(ipRateLimiter, makeOnLimitExceeded(ipRateLimiter)),
-		NewRateLimitMiddleware(userIdRateLimiter, makeOnLimitExceeded(userIdRateLimiter)),
+		NewRateLimitMiddleware(userIDRateLimiter, makeOnLimitExceeded(userIDRateLimiter)),
 	)
 
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		rawUUID := r.URL.Query().Get("uuid")
-		userId := r.Header.Get("X-User-Id")
-		ctx = reporting.SetUserIDInContext(ctx, userId)
-		if userId == "" {
-			userId = "<missing>"
+		userID := r.Header.Get("X-User-Id")
+		ctx = reporting.SetUserIDInContext(ctx, userID)
+		if userID == "" {
+			userID = "<missing>"
 		}
 		ctx = logging.AddMetaToContext(ctx,
-			slog.String("userId", userId),
+			slog.String("userID", userID),
 			slog.String("uuid", rawUUID),
 		)
 		logger := logging.FromContext(ctx)

internal/ratelimiting/ratelimiter.go
@@ -77,7 +77,7 @@ func IPKeyFunc(r *http.Request) string {
 	return fmt.Sprintf("ip: %s", withoutPort)
 }
 
-func UserIdKeyFunc(r *http.Request) string {
+func UserIDKeyFunc(r *http.Request) string {
 	userId := r.Header.Get("X-User-Id")
 	if userId == "" {
 		userId = "<missing>"

internal/ratelimiting/ratelimiter_test.go
@@ -65,9 +65,9 @@ func TestIPKeyFunc(t *testing.T) {
 	}
 }
 
-func TestUserIdKeyFunc(t *testing.T) {
+func TestUserIDKeyFunc(t *testing.T) {
 	cases := []struct {
-		userId string
+		userID string
 		key    string
 	}{
 		// Standard user ids (uuid)
@@ -81,16 +81,16 @@ func TestUserIdKeyFunc(t *testing.T) {
 		{strings.Repeat("1", 1000), "user-id: " + strings.Repeat("1", 50)},
 	}
 	for _, c := range cases {
-		t.Run(c.userId, func(t *testing.T) {
+		t.Run(c.userID, func(t *testing.T) {
 			request := &http.Request{
-				Header: http.Header{"X-User-Id": []string{c.userId}},
+				Header: http.Header{"X-User-Id": []string{c.userID}},
 			}
-			require.Equal(t, c.key, UserIdKeyFunc(request))
+			require.Equal(t, c.key, UserIDKeyFunc(request))
 		})
 	}
 	t.Run("missing", func(t *testing.T) {
 		request := &http.Request{}
-		require.Equal(t, "user-id: <missing>", UserIdKeyFunc(request))
+		require.Equal(t, "user-id: <missing>", UserIDKeyFunc(request))
 	})
 }
 

commit 54d1b284e09d7ce9a540107b703cd1019e599204

fix: Keep same log key for user id
We use this key in log-based metrics for now, so
611f554c69a51f154996021d9dcbad692cdb2203 broke that. Reverting the
change here to fix it again.

internal/ports/player_data.go
@@ -69,7 +69,7 @@ func MakeGetPlayerDataHandler(
 			userID = "<missing>"
 		}
 		ctx = logging.AddMetaToContext(ctx,
-			slog.String("userID", userID),
+			slog.String("userId", userID),
 			slog.String("uuid", rawUUID),
 		)
 		logger := logging.FromContext(ctx)