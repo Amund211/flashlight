e4c6e935222fc6ca6400f3c97ffdbdea41f369f5
Amund Eggen Svandal <aesvandal@gmail.com>
2025-08-13 23:13:56 +0200

    chore: Update comment and variable name to reflect behavior
    These seem to have been written when normalize uuid returned non-dashed
    uuids.

--- internal/strutils/uuid.go
+++ internal/strutils/uuid.go
@@ -8,13 +8,13 @@ import (
 
 const VALID_HEX_DIGITS = "0123456789abcdefABCDEF"
 
-const STRIPPED_UUID_LENGTH = 36
+const DASHED_UUID_LENGTH = 36
 
-// Removes dashes and converts all characters to lowercase
+// Converts the input string to a dashed, lowercase UUID string
 func NormalizeUUID(uuid string) (string, error) {
 	var normalized strings.Builder
 	builderCap := normalized.Cap()
-	missingCap := STRIPPED_UUID_LENGTH - builderCap
+	missingCap := DASHED_UUID_LENGTH - builderCap
 	if missingCap > 0 {
 		normalized.Grow(missingCap)
 	}
@@ -39,7 +39,7 @@ func NormalizeUUID(uuid string) (string, error) {
 			return "", fmt.Errorf("invalid character in UUID. input: '%s'", uuid)
 		}
 	}
-	if normalized.Len() != STRIPPED_UUID_LENGTH {
+	if normalized.Len() != DASHED_UUID_LENGTH {
 		return "", fmt.Errorf("normalized UUID has incorrect length. input: '%s'", uuid)
 	}
 	return normalized.String(), nil
