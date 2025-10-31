893152360729bc692215d21ceb9933776fa500a1
Amund Eggen Svandal <aesvandal@gmail.com>
2025-10-31 14:36:26 +0100

    chore: Add metrics to get tags

--- internal/app/get_tags.go
+++ internal/app/get_tags.go
@@ -10,9 +10,27 @@ import (
 	"github.com/Amund211/flashlight/internal/domain"
 	"github.com/Amund211/flashlight/internal/reporting"
 	"github.com/Amund211/flashlight/internal/strutils"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
 )
 
 type GetTags func(ctx context.Context, uuid string, apiKey *string) (domain.Tags, error)
+
+type getTagsMetricsCollection struct {
+	returnCount metric.Int64Counter
+}
+
+func setupGetTagsMetrics(meter metric.Meter) (getTagsMetricsCollection, error) {
+	returnCount, err := meter.Int64Counter("app/get_tags/return_count")
+	if err != nil {
+		return getTagsMetricsCollection{}, fmt.Errorf("failed to create return count metric: %w", err)
+	}
+
+	return getTagsMetricsCollection{
+		returnCount: returnCount,
+	}, nil
+}
 
 type tagProvider interface {
 	GetTags(ctx context.Context, uuid string, apiKey *string) (domain.Tags, error)
@@ -37,13 +55,41 @@ func buildGetTagsWithoutCache(
 func BuildGetTagsWithCache(
 	tagsByUUIDCache cache.Cache[domain.Tags],
 	provider tagProvider,
-) GetTags {
+) (GetTags, error) {
+	const name = "flashlight/app/get_tags"
+
+	meter := otel.Meter(name)
+
+	metrics, err := setupGetTagsMetrics(meter)
+	if err != nil {
+		return nil, fmt.Errorf("failed to set up metrics: %w", err)
+	}
+
 	getTagsWithoutCache := buildGetTagsWithoutCache(provider)
+
+	type trackingInfo struct {
+		cached       bool
+		success      bool
+		invalidInput bool
+	}
+
+	track := func(ctx context.Context, info trackingInfo) {
+		metrics.returnCount.Add(
+			ctx,
+			1,
+			metric.WithAttributes(
+				attribute.Bool("cached", info.cached),
+				attribute.Bool("success", info.success),
+				attribute.Bool("invalid_input", info.invalidInput),
+			),
+		)
+	}
 
 	return func(ctx context.Context, uuid string, apiKey *string) (domain.Tags, error) {
 		if !strutils.UUIDIsNormalized(uuid) {
 			err := fmt.Errorf("UUID is not normalized")
 			reporting.Report(ctx, err)
+			track(ctx, trackingInfo{success: false, invalidInput: true})
 			return domain.Tags{}, err
 		}
 
@@ -54,15 +100,17 @@ func BuildGetTagsWithCache(
 
 		key := fmt.Sprintf("uuid:%s|apiKeyHash:%s", uuid, apiKeyHash)
 
-		tags, _, err := cache.GetOrCreate(ctx, tagsByUUIDCache, key, func() (domain.Tags, error) {
+		tags, created, err := cache.GetOrCreate(ctx, tagsByUUIDCache, key, func() (domain.Tags, error) {
 			return getTagsWithoutCache(ctx, uuid, apiKey)
 		})
 		if err != nil {
 			// NOTE: GetOrCreate only returns an error if create() fails.
 			// getTagsWithoutCache handles its own error reporting
+			track(ctx, trackingInfo{success: false})
 			return domain.Tags{}, fmt.Errorf("failed to cache.GetOrCreate tags for uuid: %w", err)
 		}
 
+		track(ctx, trackingInfo{success: true, cached: !created})
 		return tags, nil
-	}
+	}, nil
 }
