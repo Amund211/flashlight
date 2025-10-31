ce91e70b15418dee9a847d7ed02602a66a74959c
Amund Eggen Svandal <aesvandal@gmail.com>
2025-11-01 00:23:05 +0100

    chore: Track found/not found in username and uuid queries
    Could be interesting for e.g. estimating rates of nicked players in
    queues.

--- internal/app/get_account_by_uuid.go
+++ internal/app/get_account_by_uuid.go
@@ -56,6 +56,7 @@ func buildGetAccountByUUIDWithoutCache(
 ) func(ctx context.Context, uuid string) (domain.Account, error) {
 	type trackingInfo struct {
 		source         string
+		found          bool
 		recovered      bool
 		providerFailed bool
 		failed         bool
@@ -70,6 +71,7 @@ func buildGetAccountByUUIDWithoutCache(
 			1,
 			metric.WithAttributes(
 				attribute.String("source", info.source),
+				attribute.Bool("found", info.found),
 				attribute.Bool("recovered", info.recovered),
 				attribute.Bool("provider_failed", info.providerFailed),
 				attribute.Bool("failed", info.failed),
@@ -82,7 +84,7 @@ func buildGetAccountByUUIDWithoutCache(
 		defer cancel()
 		providerAccount, err := provider.GetAccountByUUID(getCtx, uuid)
 		if errors.Is(err, domain.ErrUsernameNotFound) {
-			track(ctx, trackingInfo{source: "provider"})
+			track(ctx, trackingInfo{source: "provider", found: false})
 			return domain.Account{}, err
 		} else if err != nil {
 			// NOTE: accountProvider implementations handle their own error reporting
@@ -95,7 +97,12 @@ func buildGetAccountByUUIDWithoutCache(
 				// Getting the incorrect name is not critical
 				if repoAccountAge < 30*24*time.Hour {
 					// We have a valid, recent-ish account from the repository, return it
-					track(ctx, trackingInfo{source: "repository", providerFailed: true, recovered: true})
+					track(ctx, trackingInfo{
+						source:         "repository",
+						providerFailed: true,
+						recovered:      true,
+						found:          true,
+					})
 					return repoAccount, nil
 				}
 
@@ -113,7 +120,7 @@ func buildGetAccountByUUIDWithoutCache(
 			// NOTE: This error is not critical, we can still return the account
 		}
 
-		track(ctx, trackingInfo{source: "provider"})
+		track(ctx, trackingInfo{source: "provider", found: true})
 		return providerAccount, nil
 	}
 }
@@ -136,6 +143,7 @@ func BuildGetAccountByUUIDWithCache(
 	getAccountByUUIDWithoutCache := buildGetAccountByUUIDWithoutCache(provider, repo, nowFunc, metrics)
 
 	type trackingInfo struct {
+		found        bool
 		cached       bool
 		success      bool
 		invalidInput bool
@@ -146,6 +154,7 @@ func BuildGetAccountByUUIDWithCache(
 			ctx,
 			1,
 			metric.WithAttributes(
+				attribute.Bool("found", info.found),
 				attribute.Bool("cached", info.cached),
 				attribute.Bool("success", info.success),
 				attribute.Bool("invalid_input", info.invalidInput),
@@ -164,14 +173,17 @@ func BuildGetAccountByUUIDWithCache(
 		account, created, err := cache.GetOrCreate(ctx, accountByUUIDCache, uuid, func() (domain.Account, error) {
 			return getAccountByUUIDWithoutCache(ctx, uuid)
 		})
-		if err != nil {
+		if errors.Is(err, domain.ErrUsernameNotFound) {
+			track(ctx, trackingInfo{success: true, found: false})
+			return domain.Account{}, err
+		} else if err != nil {
 			// NOTE: GetOrCreate only returns an error if create() fails.
 			// getAccountByUUIDWithoutCache handles its own error reporting
 			track(ctx, trackingInfo{success: false})
 			return domain.Account{}, fmt.Errorf("failed to cache.GetOrCreate account for uuid: %w", err)
 		}
 
-		track(ctx, trackingInfo{success: true, cached: !created})
+		track(ctx, trackingInfo{success: true, found: true, cached: !created})
 		return account, nil
 	}, nil
 }
