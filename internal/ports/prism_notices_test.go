61c489bc5d1a6cdde8eecb84dcb2ce6901a2a202
Amund Eggen Svandal <aesvandal@gmail.com>
2025-12-16 09:46:47 +0100

    feat: Add notices for prism overlay wrapped

--- internal/ports/prism_notices_test.go
+++ internal/ports/prism_notices_test.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"testing/synctest"
+	"time"
 
 	"github.com/Amund211/flashlight/internal/domaintest"
 	"github.com/Amund211/flashlight/internal/ports"
@@ -16,25 +18,76 @@ import (
 func TestPrismNoticesHandler(t *testing.T) {
 	t.Parallel()
 
+	defaultTime := time.Date(2024, time.June, 1, 0, 0, 0, 0, time.UTC)
+
 	cases := []struct {
 		userID       string
 		prismVersion string
+		time         time.Time
 		responseJSON string
 	}{
 		{
 			userID:       "",
 			prismVersion: "",
 			responseJSON: `{"notices":[]}`,
+			time:         defaultTime,
 		},
 		{
 			userID:       domaintest.NewUUID(t),
 			prismVersion: "v1.10.1-dev",
 			responseJSON: `{"notices":[]}`,
+			time:         defaultTime,
 		},
 		{
 			userID:       domaintest.NewUUID(t),
 			prismVersion: "v1.11.0",
 			responseJSON: `{"notices":[]}`,
+			time:         defaultTime,
+		},
+		{
+			userID:       domaintest.NewUUID(t),
+			prismVersion: "v1.11.0",
+			responseJSON: `{"notices":[
+				{
+					"message":"Click here to view your Prism Wrapped 2025",
+					"url":"https://prismoverlay.com/wrapped",
+					"severity":"info",
+					"duration_seconds":60
+				}
+			]}`,
+			time: time.Date(2025, time.December, 1, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			userID:       domaintest.NewUUID(t),
+			prismVersion: "v1.11.0",
+			responseJSON: `{"notices":[
+				{
+					"message":"Click here to view your Prism Wrapped 2025",
+					"url":"https://prismoverlay.com/wrapped",
+					"severity":"info",
+					"duration_seconds":60
+				}
+			]}`,
+			time: time.Date(2025, time.December, 24, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			userID:       domaintest.NewUUID(t),
+			prismVersion: "v1.11.0",
+			responseJSON: `{"notices":[
+				{
+					"message":"Click here to view your Prism Wrapped 2025",
+					"url":"https://prismoverlay.com/wrapped",
+					"severity":"info",
+					"duration_seconds":60
+				}
+			]}`,
+			time: time.Date(2026, time.January, 31, 23, 59, 59, 0, time.UTC),
+		},
+		{
+			userID:       domaintest.NewUUID(t),
+			prismVersion: "v1.11.0",
+			responseJSON: `{"notices":[]}`,
+			time:         time.Date(2026, time.February, 1, 0, 0, 0, 0, time.UTC),
 		},
 	}
 
@@ -45,23 +98,28 @@ func TestPrismNoticesHandler(t *testing.T) {
 		}
 	}
 
+	// NOTE: Need to make the handler outside, since we create TTL caches inside the handler.
+	handler := ports.MakePrismNoticesHandler(testLogger, noopMiddleware)
+
 	for _, tc := range cases {
-		name := fmt.Sprintf("version='%s' userID='%s'", tc.prismVersion, tc.userID)
+		name := fmt.Sprintf("version='%s' userID='%s', time='%s'", tc.prismVersion, tc.userID, tc.time.Format(time.RFC3339))
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
-			handler := ports.MakePrismNoticesHandler(testLogger, noopMiddleware)
+			synctest.Test(t, func(t *testing.T) {
+				time.Sleep(time.Until(tc.time))
 
-			req := httptest.NewRequest("GET", "/v1/prism-notices", nil)
-			req.Header.Set("X-User-Id", tc.userID)
-			req.Header.Set("X-Prism-Version", tc.prismVersion)
+				req := httptest.NewRequest("GET", "/v1/prism-notices", nil)
+				req.Header.Set("X-User-Id", tc.userID)
+				req.Header.Set("X-Prism-Version", tc.prismVersion)
 
-			w := httptest.NewRecorder()
+				w := httptest.NewRecorder()
 
-			handler.ServeHTTP(w, req)
+				handler.ServeHTTP(w, req)
 
-			require.Equal(t, http.StatusOK, w.Code)
-			require.JSONEq(t, tc.responseJSON, w.Body.String())
-			require.Equal(t, "application/json", w.Result().Header.Get("Content-Type"))
+				require.Equal(t, http.StatusOK, w.Code)
+				require.JSONEq(t, tc.responseJSON, w.Body.String())
+				require.Equal(t, "application/json", w.Result().Header.Get("Content-Type"))
+			})
 		})
 	}
 }
